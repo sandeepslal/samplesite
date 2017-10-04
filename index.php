38dbbb17b7eb325f3af36da859520837d62dbe4d
AL <AL@DESKTOP-14MQSHF>
2017-10-04 14:40:47 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="customcss/index.css" rel="stylesheet">
+    </head>
+    <body>
+        welcome
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
